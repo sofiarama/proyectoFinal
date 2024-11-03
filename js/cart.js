eefbf999ead20605f2cabdb5ee2997a234c05f3a
Malena Etchandy <malenaetchandy@gmail.com>
2024-11-03 18:37:34 -0300

    detalless

--- js/cart.js
+++ js/cart.js
@@ -1,7 +1,7 @@
 
 document.addEventListener("DOMContentLoaded", function(){
     //Verificar si ya hay un cart o agarro el vacio
-    let cart = JSON.parse(localStorage.getItem('productInCart')) || [];
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
     const contenedor = document.getElementById('cart-items');
     contenedor.innerHTML = ''; // Vaciar el contenedor
@@ -14,22 +14,22 @@ document.addEventListener("DOMContentLoaded", function(){
         const productItem = `
         <div class="contenedor de item flex">
             <div class="imagen">
-            <img src= ${producto.image}" alt="${producto.name}" class="img-fluid">
+            <img src= ${product.image}" alt="${product.name}" class="img-fluid">
             </div>
 
             <div class="nombre y cantidad">
-            <h5>${producto.name}</h5>
+            <h5>${product.name}</h5>
                     <label for="quantity">Cantidad:</label>
-                    <input type="number" class="form-control" value="${producto.quantity}" min="1" onchange="updateSubtotal(this, ${producto.cost})">
+                    <input type="number" class="form-control" value="${product.quantity}" min="1" onchange="updateSubtotal(this, ${producto.cost})">
             </div>
 
             <div class="total">
                 <div class="moneda y precio">
-                <p>${producto.currency} ${producto.cost.toFixed(2)}</p>
+                <p>${product.currency} ${product.cost.toFixed(2)}</p>
                 </div>
 
                 <div class="subtotal">
-                <p>Subtotal: <span class="subtotal-value">${(producto.cost * producto.quantity).toFixed(2)}</span></p>
+                <p>Subtotal: <span class="subtotal-value">${(product.cost * product.quantity).toFixed(2)}</span></p>
                 </div>
             </div>
         </div>
